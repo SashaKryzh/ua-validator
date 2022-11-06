9122c2a63a59f72655d8ba6a89393913c700625e
Denys <dodenisa@gmail.com>
2022-11-06 19:11:11 +0200

    DB schema created

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,39 +3,39 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user1 = await prisma.user.upsert({
-    where: { email: "user1@test.com" },
-    update: {},
-    create: {
-      email: "user1@test.com",
-      name: "User 1",
-    },
-  });
+  // await prisma..upsert({
+  //   where: { email: "user1@test.com" },
+  //   update: {},
+  //   create: {
+  //     email: "user1@test.com",
+  //     name: "User 1",
+  //   },
+  // });
 
-  console.log(user1);
+  // console.log(user1);
 
-  await prisma.target.upsert({
-    where: { slug: "target-1" },
-    update: {},
-    create: {
-      name: "Target 1",
-      slug: "target-1",
-      verdict: {
-        create: {
-          name: "Verdict 1",
-        },
-      },
-      resources: {
-        create: [
-          {
-            name: "Resource 1",
-            link: "https://example.com",
-            main: true,
-          },
-        ],
-      },
-    },
-  });
+  // await prisma.target.upsert({
+  //   where: { slug: "target-1" },
+  //   update: {},
+  //   create: {
+  //     name: "Target 1",
+  //     slug: "target-1",
+  //     verdict: {
+  //       create: {
+  //         name: "Verdict 1",
+  //       },
+  //     },
+  //     resources: {
+  //       create: [
+  //         {
+  //           name: "Resource 1",
+  //           link: "https://example.com",
+  //           main: true,
+  //         },
+  //       ],
+  //     },
+  //   },
+  // });
 }
 
 main()
