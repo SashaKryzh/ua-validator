20a7fa373dd17c4dfcb6f63f35823f9f336bd230
Denys Churchyn <dodenisa@gmail.com>
2023-02-19 13:37:15 +0200

    pr fix

--- src/server/controller/target.controller.ts
+++ src/server/controller/target.controller.ts
@@ -48,14 +48,14 @@ export const createTargetHandler = async ({
               images: {
                 create: input.evidence.images.map((i) => ({ path: i })),
               },
-              creator: tryConnectCreator(creatorEmail),
+              creator: connectIfAny(creatorEmail),
             },
           ],
         },
         resources: {
           create: input.resources.map((r) => ({ url: r })),
         },
-        creator: tryConnectCreator(creatorEmail),
+        creator: connectIfAny(creatorEmail),
       },
     });
   } catch (e) {
@@ -125,16 +125,16 @@ async function findAvailableSlug(input: CreateTargetSchema) {
  * Connects to a user if email is provided.
  * @param creatorEmail email of the user
  */
-function tryConnectCreator(creatorEmail?: string) {
+function connectIfAny(creatorEmail?: string) {
   if (!creatorEmail) return;
 
   return {
     connectOrCreate: {
       where: {
-        email: creatorEmail,
+        email: creatormail,
       },
       create: {
-        email: creatorEmail,
+        email: creatormail,
       },
     },
   };
