54df59e19e8d4734ae4495dc6c2626787b43235d
Alexander <sashakryzh@gmail.com>
2023-04-25 21:28:59 +0200

    Updated footer, header and target page (#22)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,10 @@ export default function Header() {
     <header>
       <nav className="bg-white px-2 py-4">
         <div className="mx-auto flex max-w-screen-md flex-wrap items-center justify-between">
-          <div className="flex items-center">
-            <Link href="/" className="h-3 self-center whitespace-nowrap">
-              UA Validator
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <Button href="/add-target" variant="triatery">
-              Додати
-            </Button>
-          </div>
+          <Link href="/">UA Validator</Link>
+          <Button href="/add-target" variant="triatery">
+            Додати
+          </Button>
         </div>
       </nav>
     </header>
